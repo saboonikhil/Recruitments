cc80969cf9d7ec01634f26a4c07b013be10586e6
Pranav Karnani <pranav@Pranavs-MacBook-Pro.local>
2018-10-06 14:09:08 +0530

    refined

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,8 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { ClublistingsComponent } from './clublistings/clublistings.component';
  
 @NgModule({
