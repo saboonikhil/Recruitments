d2bf1d1b6b898c103bba0861af1383ff97f306a9
Pranav Karnani <pranav@Pranavs-MacBook-Pro.local>
2018-10-31 09:07:57 +0530

    Added analysis comp

---
+++ src/app/analysis.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from './../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnalysisService {
+  private distinct_clubs_url;
+  private backend_url;
+  private submission_url;
+  constructor(private http: HttpClient) {
+    this.backend_url = environment.backend_url;
+    this.distinct_clubs_url = this.backend_url + "/analysis/distinct";
+    this.submission_url = this.backend_url + "/analysis/submissionoverview";
+  }
+
+  distinctClubs(): Observable<any> {
+    return this.http.get(this.distinct_clubs_url);
+  }
+
+  submissions(params) : Observable<any> {
+    return this.http.post(this.submission_url,params);
+  }
+}
--- src/app/analysis/analysis.component.ts
+++ src/app/analysis/analysis.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AnalysisService } from '../analysis.service';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-analysis',
@@ -8,12 +10,54 @@ import { Component, OnInit } from '@angular/core';
 export class AnalysisComponent implements OnInit {
   submission_count = 0;
   clubs_count = 0;
-  constructor() { }
+  clubs = [];
+
+  chart = [];
+  submission = [];
+  clubNames = [];
+  constructor(private analysis:AnalysisService) { }
 
   ngOnInit() {
+    this.analysis.distinctClubs().subscribe((dataClub) => {
+      this.clubs_count = dataClub.data.length;
+      this.clubs = dataClub.data;
+      for(var i=0;i<this.clubs.length;i++) {
+        var params = {"club":this.clubs[i].club_id};
+        this.clubNames.push(this.clubs[i].name);
+        this.analysis.submissions(params).subscribe((data) => {
+          this.submission.push(data.data["count (regno)"]);
+          this.submission_count += data.data["count (regno)"];
+          if(this.submission.length == this.clubs.length) {
+            this.check();
+          }
+        }, (error: any) => {
+          console.log(error);
+        });
+      }
+    }, (error: any) => {
+      console.log(error);
+    }); 
   }
 
-  onCountoEnd(): void {
-    console.log('counto end');
-}
+  check() {
+    var options = {
+      "cutoutPercentage":0,
+      "rotation":-0.5*Math.PI,
+      "animation.animateRotate":true
+    };
+
+    var data = {
+      datasets: [{
+          data: this.submission,
+          backgroundColor: ["#0074D9", "#FF4136", "#AAAAAA"]
+      }],
+      labels: this.clubNames
+    };
+  
+    this.chart = new Chart("canvas",{
+      type: 'pie',
+      data: data,
+      options: options
+  });
+  }
 }
